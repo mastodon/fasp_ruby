6eee14e9f9becd7c89705fbf11c0911b5168cc9c
David Roetzel <david@roetzel.de>
2025-05-06 14:34:22 +0200

    Upgrade debug provider to tailwind 4

--- debug_fasp/app/controllers/application_controller.rb
+++ debug_fasp/app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  default_form_builder FaspBase::FormBuilder
+
   private
 
   # Currently ever user can only have one server but this will probably
