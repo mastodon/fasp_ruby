b28bc2dddc0af8d2c9b07f50a786677901786695
David Roetzel <david@roetzel.de>
2026-07-31 12:36:06 +0200

    Require `Request` late
    So users of this engine can enable httpx's webmock integration
    before httpx is first initialised.

--- fasp_base/lib/fasp_base/engine.rb
+++ fasp_base/lib/fasp_base/engine.rb
@@ -5,5 +5,12 @@ require "linzer"
 module FaspBase
   class Engine < ::Rails::Engine
     isolate_namespace FaspBase
+
+    # Require `Request` late in the boot process
+    # to enable users to install httpx's webmock
+    # integration before httpx is initialized.
+    initializer "fasp_base.require_request" do
+      require "fasp_base/request"
+    end
   end
 end
